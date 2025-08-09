1baae5c25480d520d844e187de20835f36766346
YamatoRyuma <gamerliu2004@gmail.com>
2025-08-09 11:59:28 +0700

    Aqours part 10

--- app/src/main/java/protection/member/aqours/fragment/person/PersonPhotoActivity.kt
+++ app/src/main/java/protection/member/aqours/fragment/person/PersonPhotoActivity.kt
@@ -3,21 +3,13 @@ package protection.member.aqours.fragment.person
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import protection.member.aqours.databinding.ActivityPersonPhotoBinding
-import protection.member.aqours.list_character.ListCharacterData
 
 class PersonPhotoActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityPersonPhotoBinding
-    private lateinit var recyclerView: RecyclerView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPersonPhotoBinding.inflate(layoutInflater)
+        val binding = ActivityPersonPhotoBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        recyclerView = binding.recyclerViewPersonal
     }
 
     override fun onSupportNavigateUp(): Boolean {
